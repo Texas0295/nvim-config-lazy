a147d61e2e43766ec6bfbd181f0834605b63a406
Texas0295 <Texas0295@outlook.com>
2024-10-09 21:09:56 +0800

    installation path was changed

--- lua/texas0295/configs/dap-configs/c-dap.lua
+++ lua/texas0295/configs/dap-configs/c-dap.lua
@@ -3,7 +3,7 @@ local dap = require("dap")
 dap.adapters.cppdbg = {
   id = 'cppdbg',
   type = 'executable',
-  command = 'E:\\developTools\\cpptools-windows-x64\\extension\\debugAdapters\\bin\\OpenDebugAD7.exe',
+  command = 'D:\\developTools\\cpptools-windows-x64\\extension\\debugAdapters\\bin\\OpenDebugAD7.exe',
   options = {
     detached = false
   }
