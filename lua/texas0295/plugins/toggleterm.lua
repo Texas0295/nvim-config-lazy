dee8d911641193360f3d6b0acae4f6e2f8048c4c
Texas0295 <Texas0295@outlook.com>
2025-04-29 19:49:22 +0800

    roll back

--- lua/texas0295/plugins/toggleterm.lua
+++ lua/texas0295/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return{
     config = function()
         require("toggleterm").setup({
             size = 8,
-            open_mapping = [[<leader>t]],
+            open_mapping = [[<c-t>]],
             direction = "float",
             shell = "fish",
             dir = vim.fn.expand("%:p:h"),
